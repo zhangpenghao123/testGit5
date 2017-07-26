49fb9b6edbff522398373fc97302a8dde299bd0a
zhangpenghao123 <1028701252@qq.com>
2017-07-26 16:44:13 +0800

    哈哈哈

---
+++ test_git/src/com/bwie/test/TsetHello.java
@@ -0,0 +1,14 @@
+package com.bwie.test;
+
+public class TsetHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello");
+       
+	}
+
+}
